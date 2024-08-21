21b934bc0e9d34ac64340f1fc9a81855ff008689
aaaaali1 <2281178508@qq.com>
2024-08-21 19:11:57 +0800

    Add files via upload

--- 计算器.cpp
+++ 计算器.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main()
 {
 	double a, c;
-	char b;
-	cout << "请输入第一个数字" << endl;
+	char b;//定义运算符
+	cout << "请输入第一个数字" << endl;//输入运算数字
 	cin >> a;
 	cout << "请输入运算符" << endl;
 	cin >> b;
 	cout << "请输入第二个数字" << endl;
 	cin >> c;
-	switch(b) {
+	switch(b) {//识别运算符。以选择正确对应的运算方式和输出内容
 case '+':
 	cout << a << '+' << c << '=' << (a + c) << endl;
 	break;
@@ -24,10 +24,10 @@ case '/':
 	if (c != 0)
 		cout << a << '/' << c << '=' << (a / c) << endl;
 	else
-		cout << "输入有误" << endl;
+		cout << "输入有误" << endl;//注意除数不为0的除法原则
 		break;
 default:
-	cout << "没有此符号" << endl;
+	cout << "没有此符号" << endl;//表明运算符输入错误的情况
 	break;
 	}
 
